cd1a5a58c2da68ceba3866a31cd98bf9fe831904
LJPileggi <l.pileggi1@studenti.unipi.it>
2021-10-18 12:21:22 +0200

    nn_unit class file created. Still needs to implement the __str__ method.

---
+++ nn_unit.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class nn_unit:
+    """Object class for the NN unit
+    params:
+     - x: input vector; class type: numpy.ndarray;
+     - w_i: weight vector; class type: numpy.ndarray;
+     - f_i: activation function; class type: function.
+    
+    properties:
+     - net_i: scalar product between x and w_i; class type: float;
+     - o_i: output vector; class type: float or numpy.ndarray.
+    """
+    typecode = 'f'
+    def __init__(self, x, w_i, f_i):
+        if not isinstance(type(x), np.ndarray):
+            raise TypeError(f'TypeError: argument x must be <{np.ndarray}>, not <{type(x)}>')
+        self.x = x
+        if not isinstance(type(w_i), np.ndarray):
+            raise TypeError(f'TypeError: argument w_i must be <{np.ndarray}>, not <{type(w_i)}>')
+        self.w_i = w_i
+        self.f_i = f_i
+
+    @property
+    def net_i(self):
+        return float((self.x * self.w_i).sum())
+
+    @property
+    def o_i(self):
+        return self.f(self.net_i)
+
