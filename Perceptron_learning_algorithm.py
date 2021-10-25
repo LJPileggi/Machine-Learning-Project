05efa025ef2784135ff3789ff7e0133e5b4746b6
LJPileggi <l.pileggi1@studenti.unipi.it>
2021-10-25 23:42:06 +0200

    Tweak added to Perceptron_learning_algorithm: control parameter added for max number of iterations.

--- Perceptron_learning_algorithm.py
+++ Perceptron_learning_algorithm.py
@@ -1,13 +1,19 @@
 #pylint: disable = C0114, C0103
 
+from math import floor
+
 import numpy as np
 
 from activation_functions import threshold_func
 
-def Perceptron_learn_alg(x, w_init, d, eta=0.5, thr=0.01):
+def Perceptron_learn_alg(x, w_init, d, eta=0.5, thr=0.01, N_max=1000):
     """
     Implements the Perceptron learning algorithm to minimise
-    misclassifications.
+    misclassifications. Starts from an initialised weight
+    vector: if no misclassification is present, returns
+    such vector itself; otherwise, updates weight vector
+    with a correction term, until changes go below a
+    specified threshold.
 
     params:
      - x: input vector; class type: numpy.ndarray;
@@ -16,7 +22,10 @@ def Perceptron_learn_alg(x, w_init, d, eta=0.5, thr=0.01):
      - eta: learning rate, must be between 0 and 1; default value: 0.5;
      class type: float;
      - thr: learning threshold, if Dw < thr then returns; must be
-     positive; default value: 0.01; class type: float.
+     positive; default value: 0.01; class type: float;
+     - N_max: control parameter, sets the maximum number of iterations
+     handlable; must be positive integer; default value: 1000;
+     class type: int.
 
      returns:
       - numpy.ndarray of optimal weight values.
@@ -29,15 +38,19 @@ def Perceptron_learn_alg(x, w_init, d, eta=0.5, thr=0.01):
     if not (d == 1) | (d == -1):
         raise ValueError('ValueError: d must be either 1 or -1')
     if not (eta >= 0.) | (eta <= 1.):
-        raise ValueError('ValueError: value of eta must fall between 0. and 1.')
+        raise ValueError('ValueError: eta must fall between 0. and 1.')
     if thr < 0.:
-        raise ValueError('ValueError: value of thr must not be less than 0.')
+        raise ValueError('ValueError: thr must be positive')
+    if N_max < 0:
+        raise ValueError('ValueError: N_max must be positive integer')
     #actual algorithm
     w_old = w_init
-    while True:
+    i = floor(N_max)
+    while i > 0:
         if d == threshold_func(x, w_old, boolean=False):
             return w_old
         w_new = w_old + eta*d*x
         d_w = w_new - w_old
         if np.all((d_w < thr) & (d_w > -thr)):
             return w_new
+        i -= 1
