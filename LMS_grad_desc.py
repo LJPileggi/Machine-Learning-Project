6729cc7b14edeef17d5562dbab2a4b7f646129d3
Gabriele Tedeschi <gabriele_tedeschi@outlook.com>
2021-11-09 02:49:03 +0100

    Implementato batch e online su singola nnunit
    Si nota che l'algoritmo batch, in presenza di errori, converge in fretta
    a un errore piccolo, ma impiega molto tempo a scendere ulteriormente.
    Il che però non ha senso, mi aspetterei che batch gli errori si appino da soli.
    Forse il batch è troppo piccolo? Oppure l'eta troppo grande fa rimbalzare intorno
    al minimo locale senza mai entrarci? probabilmente con un eta decrescente
    si comporterebbe meglio

--- LMS_grad_desc.py
+++ LMS_grad_desc.py
@@ -1,18 +1,104 @@
 #pylint: disable = C0114, C0103, R0913
 
 from math import floor
-
+import random as rand
 import numpy as np
+from nn_unit import nn_unit
 
-def Error(x, w, y):
+
+def _LMS_step(unit, x, y):
     """
+    Implements gradient descent algorithm on lms error.
+    Updates weight vectors until convergence is reached
+    or lms error goes below a fixed threshold (thr).
+
+    params:
+     - x: input vector; class type: numpy.ndarray;
+     - y: output vector; class type: float;
+      class type: float;
+
+     returns:
+      - delta_w; class type: numpy.ndarray.
+    """
+    return (y - unit.out(x))*unit.out_prime(x)*x
+
+def _mean_square_error(TS, unit):
+        errors = [(unit.out(x) - y)**2 for (x, y) in TS]
+        mse = sum(errors)/len(errors)
+        #print("mse", mse)
+        return mse
+
+def grad_descent_LMS(TS, eta=0.01, thr=0.001, N_max=10000, batch=False):
+    """
+    Implements gradient descent algorithm on lms error.
+    Updates weight vectors until convergence is reached
+    or lms error goes below a fixed threshold (thr).
+
+    params:
+     - TS: the training set, consisting in a list of couples (pattern, label)
+     - eta: learning rate, must lie between 0 and 1; default value: 0.5;
+     class type: float;
+     - thr: learning threshold, if Err < thr then returns; must be
+     positive; default value: 0.01; class type: float;
+     - N_max: control parameter, sets the maximum number of iterations
+     handlable; must be positive integer; default value: 1000;
+     class type: int.
+
+     returns:
+      - updated weight vectors; class type: numpy.ndarray.
+    """
+    unit = nn_unit(3)
+    print(unit.w)
+    epochs = 1
+    while epochs <= N_max and _mean_square_error(TS, unit) > thr:
+       #print("epoch ", epochs)
+       if batch == False:
+            for x, y in TS:
+                delta_w = _LMS_step(unit, x, y)
+                unit.update(eta*delta_w)
+                epochs += 1
+       else:
+            total_delta_w = np.zeros(3)
+            for x, y in TS:
+                delta_w = _LMS_step(unit, x, y)
+                total_delta_w = total_delta_w + delta_w
+            unit.update(eta*(1/50)*total_delta_w)
+            epochs += 1
+    return unit.w
+
+
+
+TS = []
+b = 5
+for i in range(50):
+    x = rand.random()
+    y = rand.random()
+    out = 3*x + y +10 + 0.1*rand.gauss(0., 1)
+    TS.append((np.array([1., x, y]), out))
+
+
+
+    """
+    w_old = w_init
+    i = floor(N_max)
+    while i > 0:
+        if Error(x, w_old, y) < thr:
+            return w_old
+        w_new = w_old - eta*D_Error(x, w_old, y)
+        w_old = w_new
+        i -= 1
+    return w_old"""
+
+"""
+def Error(x, w, y):
+    
     Empirical error for least mean squares regression.
 
     params:
      - x: input vector; class type: numpy.ndarray;
      - w: weight vector; class type: numpy.ndarray;
      - y: output vector; class type: float.
-    """
+    
     #error handling
     if not isinstance(type(x), np.ndarray):
         raise TypeError(f'TypeError: argument x must be <{np.ndarray}>, not <{type(x)}>')
@@ -25,14 +111,14 @@ def Error(x, w, y):
     return err
 
 def D_Error(x, w, y):
-    """
+    
     Gradient of mean square error.
 
     params:
      - x: input vector; class type: numpy.ndarray;
      - w: weight vector; class type: numpy.ndarray;
      - y: output vector; class type: float.
-    """
+    
     #error handling
     if not isinstance(type(x), np.ndarray):
         raise TypeError(f'TypeError: argument x must be <{np.ndarray}>, not <{type(x)}>')
@@ -44,51 +130,8 @@ def D_Error(x, w, y):
     D_err = ((x*w).sum() - y)*x
     return D_err
 
-def LMS_grad_desc(x, w_init, y, eta=0.5, thr=0.01, N_max=1000):
-    """
-    Implements gradient descent algorithm on lms error.
-    Updates weight vectors until convergence is reached
-    or lms error goes below a fixed threshold (thr).
 
-    params:
-     - x: input vector; class type: numpy.ndarray;
-     - w_init: initialised weight vector; class type: numpy.ndarray;
-     - y: output vector; class type: float;
-     - eta: learning rate, must lie between 0 and 1; default value: 0.5;
-     class type: float;
-     - thr: learning threshold, if Err < thr then returns; must be
-     positive; default value: 0.01; class type: float;
-     - N_max: control parameter, sets the maximum number of iterations
-     handlable; must be positive integer; default value: 1000;
-     class type: int.
 
-     returns:
-      - updated weight vectors; class type: numpy.ndarray.
-    """
-    #error handling
-    if not isinstance(type(x), np.ndarray):
-        raise TypeError(f'TypeError: argument x must be <{np.ndarray}>, not <{type(x)}>')
-    if not isinstance(type(w_init), np.ndarray):
-        raise TypeError(f'TypeError: argument w_init must be <{np.ndarray}>, not <{type(w_init)}>')
-    if not isinstance(type(y), float):
-        raise TypeError(f'TypeError: argument y must be <{float}>, not <{type(y)}>')
-    if not (eta >= 0.) | (eta <= 1.):
-        raise ValueError('ValueError: eta must fall between 0. and 1.')
-    if thr < 0.:
-        raise ValueError('ValueError: thr must be positive')
-    if N_max < 0:
-        raise ValueError('ValueError: N_max must be positive integer')
-    w_old = w_init
-    i = floor(N_max)
-    while i > 0:
-        if Error(x, w_old, y) < thr:
-            return w_old
-        w_new = w_old - eta*D_Error(x, w_old, y)
-        w_old = w_new
-        i -= 1
-    return w_old
-
-"""
 TO BE COMPLETED
 def LMS_grad_desc_batch(x, w_init, y, eta=0.5, thr=0.01, N_max=1000):
     
@@ -137,23 +180,9 @@ def LMS_grad_desc_batch(x, w_init, y, eta=0.5, thr=0.01, N_max=1000):
         w_old = w_new
         i -= 1
     return w_old
-    """"
+    """
 
 
-"""def mean_square_error = Avg {Error(x, unit, y) for x in TS}
-def LMS_grad_descent(TS, eta=0.5, thr=0.01, N_max=1000, batch=false)
-    random_small_weights = Numpy.randomVectorLEssThenSomething
-    unit = NNunit(random_small_weights, )
-    while mean_square_error(TS, unit) > thr:
-       if batch = false:
-            for x, y in TS:
-                Dw = step(unit, x, y)
-                unit.update(Dw)
-       else
-            for x,y in Ts:
-                Dw = step(unit, x, y)
-                totalDw += Dw
-            unit.update(totalDw)"""
             
             
         
