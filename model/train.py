86da1df4dfc24a3e977a014f8cd5811da1637f66
Gabriele Tedeschi <gabriele_tedeschi@outlook.com>
2022-01-02 16:59:51 +0100

    minor fixes

--- model/train.py
+++ model/train.py
@@ -64,10 +64,10 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
         #history['gradients'] = [ [ [] for layer in layers ]  for n in range(max_fold)]
         history['weight_changes'] = [ []  for n in range(max_fold)]
         history['val_step'] = check_step
-        if var_eta:
-            history['name'] = f"{layers}_{batch_size}_{eta}_var_{lam}_{alpha}"
-        else:
+        if eta_decay == -1:
             history['name'] = f"{layers}_{batch_size}_{eta}_nonvar_{lam}_{alpha}"
+        else:
+            history['name'] = f"{layers}_{batch_size}_{eta}_var_{lam}_{alpha}"
         history['hyperparameters'] = (layers, batch_size, eta, lam, alpha)
         history['mean']      = 0
         history['variance']  = 0
@@ -103,7 +103,7 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
                         error = pattern[1] - out
                         nn.backwards(error)
                         #we are updating with eta/TS_size in order to compute LMS, not simply LS
-                    if eta_decay == .1:
+                    if eta_decay == -1:
                         nn.update_weights(eta/len(whole_TR), lam, alpha)
                     else:
                         nn.update_weights((0.99*np.exp(-i/eta_decay)+0.01)*eta/len(whole_TR), lam, alpha)
@@ -119,13 +119,14 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
                     history['validation'][n_fold].append(val_err)
                     print (f"{n_fold}_fold - {i} - {history['name']}: {train_err} - {val_err}")
                     #compute store and print weights change
-                    total_change = 0
+                    wc = 0
                     newWeights = nn.get_weights()
-                    for oldW, newW in zip(oldWeights, newWeights):
-                        total_change += np.mean(np.abs((oldW-newW)/oldW))
+                    wc = np.mean(
+                        [np.abs((oldW-newW)/oldW) for oldW, newW in zip(oldWeights, newWeights)] 
+                        )
                     oldWeights = newWeights
-                    history['weight_changes'][n_fold].append(total_change)
-                    print(f"total change: {total_change}")
+                    history['weight_changes'][n_fold].append(wc)
+                    print(f"total change: {wc}")
                     #stopping criteria
                     if (np.allclose(val_err, 0, atol=epsilon)):
                         break
