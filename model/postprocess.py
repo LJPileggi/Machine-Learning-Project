957a0d367c3069c0f54379501c34ec754f8f1b20
Wallace4 <mikde4@gmail.com>
2022-01-04 20:35:06 +0100

    test with retraining over, fixed postprocess

--- model/postprocess.py
+++ model/postprocess.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 # used in training: computes NN.forward
 def MSE_over_network(batch, NN):
@@ -69,3 +71,38 @@ def create_graph (history, graph_path, filename):
     # plt.legend()
     # plt.savefig(os.path.join(val_path, filename))
     # plt.clf()
+
+
+def create_final_graph (history, graph_path, filename):
+    plt.title(f'Train and Test error - Final Test MEE: {history["testing"]:.2f}')
+    plt.xlabel('Epochs')
+    plt.yscale('log')
+    plt.ylabel('Loss')
+    train = history['training']
+    epochs = range(len(train))
+    plt.plot(epochs, train, linestyle='-', label=f'Training loss')
+    val = history['validation']
+    epochs = [x*history['val_step'] for x in range(len(val))]
+    plt.plot(epochs, val, linestyle='--', label=f'Test loss')
+
+    # #val_path = os.path.join(graph_path, 'validation')
+    train_path = os.path.join(graph_path, 'training')
+    if (not os.path.exists(train_path)):
+        os.makedirs(train_path)
+    plt.legend()
+    plt.savefig(os.path.join(train_path, filename))
+    plt.clf()
+
+    plt.title(f'Average Weights change')
+    plt.xlabel('Epochs')
+    plt.yscale('log')
+    plt.ylabel('Values')
+    wc = history['weight_changes']
+    epochs = [x*history['val_step'] for x in range(len(val))]
+    plt.plot(epochs, wc, linestyle='-.', label=f'WC loss')
+    grad_path = os.path.join(graph_path, 'gradients')
+    if (not os.path.exists(grad_path)):
+        os.makedirs(grad_path)
+    plt.legend()
+    plt.savefig(os.path.join(grad_path, filename))
+    plt.clf()
--- model/test.py
+++ model/test.py
@@ -1,7 +1,8 @@
 #file dove loadare un modello, una config, (che ha dentro la location del file di test), e che calcoli i valori di ogni elemento
 
 from dataloader import DataLoader
-from postprocess import MSE_over_network, empirical_error
+from postprocess import MSE_over_network, empirical_error, create_final_graph
+from MLP import MLP
 
 import random
 import os
@@ -11,11 +12,15 @@ import time
 
 import numpy as np
 
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+
 #it reads as nonlocal variables: dl, activation, checkstep, maxstep, epsilon
-def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
+def retrain(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
     try:
         #accessing data
-        input_size = dl.get_input_size ()
+        input_size = dl.get_input_size ('train')
 #        whole_TR = dl.get_partition_set('train') #ritornano in auge
 #        whole_VL = dl.get_partition_set('val')
         
@@ -30,7 +35,7 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
         threshold   = global_confs["wc_threshold"]
 
         #set local configuration
-        layers      = local_confs["layers"]
+        layers      = local_confs["hidden_units"]
         batch_size  = local_confs["batch_size"]
         eta_decay   = local_confs["eta_decay"]#if == -1 no eta decay; 25 should be fine
         eta         = local_confs["eta"]
@@ -62,10 +67,8 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
         oldWeights = nn.get_weights()
 
         #accessing the data of the k_fold
-        whole_TR = dl.get_partition_set (train_idx)
-        whole_TS = dl.get_partition_set (test_idx)
-            
-        print(f"partito un ciclo di cross val - {n_fold}")
+        whole_TR = dl.get_tag_set ('train')
+        whole_TS = dl.get_tag_set ('test')
             
         train_err = MSE_over_network (whole_TR, nn)
         history['training'].append(train_err)
@@ -75,7 +78,7 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
 
         low_wc = 0
         for i in range (max_step):
-            for current_batch in dl.dataset_partition(train_idx, batch_size):
+            for current_batch in dl.dataset_partition(range(len(whole_TR)), batch_size, 'train'):
                 for pattern in current_batch:
                     out = nn.forward(pattern[0])
                     error = pattern[1] - out
@@ -96,7 +99,7 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
                 #compute store and print validation error
                 val_err = empirical_error(whole_TS, nn, metric)
                 history['validation'].append(val_err)
-                print (f"{n_fold}_fold - {i} - {history['name']}: {train_err} - {val_err}")
+                print (f"{i} - Done")
                 #compute store and print weights change
                 wc = 0
                 newWeights = nn.get_weights()
@@ -110,15 +113,17 @@ def train(dl, global_confs, local_confs, output_path, graph_path, seed=4444):
                     low_wc = 0 
                 if (np.allclose(val_err, 0, atol=epsilon) or low_wc >= patience):
                     break
+
+        input("Sicuro Sicuro?")
         
-        history['testing'] = empirical_error (whole_VL, nn, metric)
-        print(f"accuracy - {history['name']}: {(history['testing'][n_fold])}")
+        history['testing'] = empirical_error (whole_TS, nn, metric)
+        print(f"accuracy - {history['name']}: {history['testing']}")
 
         ### saving model and plotting loss ###
         nn.save_model(os.path.join(output_path, f"model_{history['name']}.h5"))
 
         ### plotting loss ###
-        create_graph(history, graph_path, f"training_loss_{history['name']}.png")
+        create_final_graph(history, graph_path, f"training_loss_{history['name']}.png")
         return history
     except KeyboardInterrupt:
         print('Interrupted')
@@ -130,19 +135,24 @@ def main():
     parser.add_argument('--config_path',
                         help='path to config file')
     parser.add_argument('--model_path',
-                        help='path to a model to ')
+                        help='path to a model to ') #not implemented yet
     parser.add_argument('--seed',
                         help='random seed')
     parser.add_argument('--retraining', action='store_true',
                         help='if you want to retrain the model with all the train set after a successfull kfold-crossval')
     parser.add_argument('--publish', action='store_true',
-                        help='if you want to generate a csv file with the results and dont confront them')
+                        help='if you want to generate a csv file with the results and dont confront them') #not implemented yet
     parser.set_defaults(seed=int(time.time()))
     parser.set_defaults(retraining=False)
     parser.set_defaults(publish=False)
     parser.set_defaults(config_path=None)
     parser.set_defaults(model_path=None)
     args = parser.parse_args()
+
+    choice = input ("ATTENZIONE! Se stai avviando questo programma stai facendo model assesment, questo significa che non potrai tornare ad allenare i dati dopo questo momento. Sei sicuro di voler avviare questo programma? - ")
+    if (choice == 'n'):
+        exit()
+    
     if (args.config_path != None):
         config = json.load(open(args.config_path))
 
@@ -151,8 +161,9 @@ def main():
         set_seed(seed)
     
         dl = DataLoader (seed)
+        dl.load_data(config["test_set"], config["input_size"], config["output_size"], config.get("preprocessing"), 'test')
 
-        if (retraining):
+        if (args.retraining):
             output_path = os.path.abspath(config["output_path"])
             output_path = os.path.join(output_path, 'FINAL_MODEL')
             print(output_path)
@@ -164,14 +175,14 @@ def main():
             if (not os.path.exists(graph_path)):
                 os.makedirs(graph_path)
                 
-            dl.load_data(config_["train_set"], config_["input_size"], config["output_size"], config.get("preprocessing"))
+            dl.load_data(config["train_set"], config["input_size"], config["output_size"], config.get("preprocessing"), "train")
             ### loading CONSTANT parameters from config ###
             global_conf = config["model"]["global_conf"]
 
             ### loding hyperparameters from config ###
             hyperparameters = config["model"]["hyperparameters"]
             #each configuration is a triple: datas, global confs and local confs
-            h = retrain(dl, global_conf, hyperparameters, output_path, graph_path, seed)
-            
+            retrain(dl, global_conf, hyperparameters, output_path, graph_path, seed)
 
-        dl.load_data(config_["test_set"], config_["input_size"], config["output_size"], config.get("preprocessing"))
+if __name__ == '__main__':
+    main()
